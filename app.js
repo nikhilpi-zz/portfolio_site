475073f63b364be01fbc35201c8db0a5984f9af9
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-03-08 23:57:54 -0600

    fixing ports

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ var app = express();
 require('./config/express')(app, config);
 
 app.listen(config.port, function () {
-  console.log('Express server listening on port ' + config.port);
+  console.log('Express server listening on port ' + process.env.PORT || config.port);
 });
 
