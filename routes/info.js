d0af54170e86a5b006b2ec11fc43393360a45d90
Nikhil Pai <nikhil.h.pai@gmail.com>
2017-12-03 16:07:39 -0800

    rip es6

--- routes/info.js
+++ routes/info.js
@@ -3,47 +3,31 @@ module.exports = {
       {
         image_src: '/images/shogun.png',
         title: 'Shogun',
-        description: `
-          I currently work at a small startup called Shogun. We're a financial technology 
-          company looking to optimize the average American's largest asset, their home.
-          We're hiring!`,
+        description: "I currently work at a small startup called Shogun. We're a financial technology company looking to optimize the average American's largest asset, their home. We're hiring!",
         link: 'http://www.shoguninc.com/'
       },
       {
         image_src: '/images/slack.png',
         title: 'Slack',
-        description: `
-         I was part of Slack first intern class in the summer of 2015. As a front-end engineer, 
-         I built out the user groups feature and did some product management on the enterprise product.`,
+        description: "I was part of Slack first intern class in the summer of 2015. As a front-end engineer, I built out the user groups feature and did some product management on the enterprise product.",
         link: 'https://slackhq.com/introducing-user-groups-5a17e4531d32'
       },
       {
         image_src: '/images/delta.png',
         title: 'Delta Lab @ Northwestern University',
-        description: `
-         As an undergraduate at Northwestern University, I researched in the Delta Lab under the 
-         supervision of Professor Haoqi Zhang. We explored methods of breaking down complex projects 
-         into actionable plans through crowdsourcing.
-        `,
+        description: "As an undergraduate at Northwestern University, I researched in the Delta Lab under the supervision of Professor Haoqi Zhang. We explored methods of breaking down complex projects into actionable plans through crowdsourcing.",
         link: 'http://dtr.northwestern.edu/'
       },
       {
         image_src: '/images/knight.png',
         title: 'Knight Lab @ Northwestern University',
-        description: `
-          As a technology fellow at the Knight Lab, I researched novel methods of adding context to 
-          long-form articles using NLP and knowledge graphs. The Knight Lab is a journalism technology 
-          lab funded by the Knight Foundation.
-        `,
+        description: "As a technology fellow at the Knight Lab, I researched novel methods of adding context to long-form articles using NLP and knowledge graphs. The Knight Lab is a journalism technology lab funded by the Knight Foundation.",
         link: 'https://knightlab.northwestern.edu/'
       },
       {
         image_src: '/images/groupon.png',
         title: 'Groupon',
-        description: `
-          In the summer of 2014, I interned as a web developer on Groupon's homepage team. I built out 
-          features that resulted in 2 million dollars in marginal monthly revenue. 
-        `,
+        description: "In the summer of 2014, I interned as a web developer on Groupon's homepage team. I built out features that resulted in 2 million dollars in marginal monthly revenue.",
         link: 'www.groupon.com'
       }
     ],
